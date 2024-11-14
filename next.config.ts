0ab3f1fff00711e34dd26b459500c7de4bbeb8a0
vansh.shah <vanshshah174@gmail.com>
2024-11-13 20:51:20 -0500

    PPR implemented startup page

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,15 @@ images:{
       hostname: '*'
     }
 ]
-}
+},
+  experimental : {
+    ppr:'incremental'
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity:true,
+    buildActivityPosition: 'bottom-right',
+  },
 };
 
 export default nextConfig;

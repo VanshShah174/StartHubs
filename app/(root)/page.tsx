ea2c2e58665fc8fa8fc6f1f3939c5d0461ec00e5
vansh.shah <vanshshah174@gmail.com>
2024-11-28 20:42:29 -0500

    Author_Authentication_Flow maintained with nextAuth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import SearchForm from "../../components/SearchForm";
 import StartupCard ,{ StartupTypeCard} from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({searchParams} :
    {
@@ -14,6 +15,10 @@ export default async function Home({searchParams} :
     // const posts = await client.fetch(STARTUPS_QUERY);
 
     const params = { search :  query || null };
+
+    const session = await auth();
+
+    console.log(session?.id)
 
     const { data : posts } = await sanityFetch({ query:STARTUPS_QUERY , params} );
 

571d57804fa684ac5160da07d08578ad19637f01
vansh.shah <vanshshah174@gmail.com>
2024-12-19 19:19:59 -0500

    ui of the submit form implemented

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@/auth'
+import StartupForm from '@/components/StartupForm'
+import { url } from 'inspector';
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const Page = async () => {
+
+    const session = await auth();
+
+    if(!session) redirect("/");
+
+  return (
+    <>
+    <section className='pink_container !min-h-[230px]'>
+    <h1 className='heading'>
+    Submit your startup
+    </h1>
+    </section>
+    <StartupForm />
+    
+    </>
+  )
+}
+
+export default Page
